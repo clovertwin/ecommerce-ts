e2f1461ec680d852f076d8bd4ea354965963d229
Shaun Carr <shaunc138@gmail.com>
2022-08-13 00:18:08 -0400

    Setup lib client

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <h1 className="text-center font-extrabold text-9xl">Hello world</h1>
     </div>
   );
 };
