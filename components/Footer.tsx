bebeb4ab04c1f3d381886402f6455c60aaa15af6
Shaun Carr <shaunc138@gmail.com>
2022-08-14 18:23:18 -0400

    Build Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,15 @@
+import { AiOutlineTwitter, AiOutlineInstagram } from "react-icons/ai";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex flex-col justify-center items-center mt-20 py-8 text-neutral-600">
+      <p className="font-semibold">2022 AccessoryShack All rights reserved</p>
+      <p className="flex text-4xl mt-5">
+        <AiOutlineTwitter className="transition ease-in-out duration-500 hover:text-black hover:cursor-pointer mr-5" />
+        <AiOutlineInstagram className="transition ease-in-out duration-500 hover:text-black hover:cursor-pointer" />
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,11 +8,15 @@ const Navbar = () => {
 
   return (
     <header className="sticky top-0 z-30 flex justify-between items-center bg-neutral-50 shadow-md px-7 py-3">
-      <p className="font-bold text-lg">
+      <p className="font-bold text-lg text-neutral-600 transition-transform duration-500 ease-in-out hover:text-black">
         <Link href="/">AccessoryShack</Link>
       </p>
 
-      <button type="button" className="relative flex text-2xl cursor-pointer">
+      <button
+        type="button"
+        onClick={() => setShowCart((prev) => !prev)}
+        className="relative flex text-2xl cursor-pointer text-neutral-600 transition-transform duration-500 ease-in-out hover:scale-125 hover:text-black"
+      >
         <MdOutlineShoppingCart />
         <span className="absolute -right-2 -top-1 text-xs font-semibold bg-red-500 text-neutral-50 w-4 h-4 leading-tight rounded-full text-center">
           0
