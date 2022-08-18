ee03cd708cdfb197a083e6cf51660ba4efc542b3
Shaun Carr <shaunc138@gmail.com>
2022-08-18 12:08:47 -0400

    Add to FooterBanner

--- components/FooterBanner.tsx
+++ components/FooterBanner.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Banner } from "../typings";
 
 interface Props {
@@ -7,6 +8,7 @@ interface Props {
 
 const FooterBanner = ({
   bannerData: {
+    product,
     buttonText,
     smallText,
     midText,
@@ -18,8 +20,29 @@ const FooterBanner = ({
 }: Props) => {
   return (
     <div className="mt-20">
-      <div className="bg-red-500 md:px-10 md:h-[500px] md:rounded-2xl">
-        <h2>{buttonText}</h2>
+      <div className="bg-red-500 flex flex-col items-start justify-between h-[500px] p-5 md:px-10 md:h-[500px] md:rounded-2xl">
+        <div>
+          <h3 className="text-4xl text-neutral-50">{midText}</h3>
+          <h2 className="text-8xl text-neutral-50 font-bold tracking-tighter">
+            {largeText1}
+          </h2>
+          <h2 className="text-8xl text-neutral-50 font-bold tracking-tighter">
+            {largeText2}
+          </h2>
+        </div>
+        <div>
+          <p className="text-neutral-50 text-2xl">{smallText}</p>
+          <h3 className="text-6xl text-neutral-50 font-bold">{discount}</h3>
+          <p className="text-neutral-50 text-2xl">{saleTime}</p>
+          <Link href={`/product/${product}`}>
+            <button
+              type="button"
+              className="px-7 py-2 rounded-full bg-neutral-50 text-red-500 text-2xl font-semibold mt-5 transition-transform ease-in-out duration-300 hover:scale-110"
+            >
+              {buttonText}
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -8,20 +8,30 @@ interface Props {
   bannerData: Banner;
 }
 
-const HeroBanner = ({ bannerData }: Props) => {
-  const imageProps = useNextSanityImage(client, bannerData.image);
+const HeroBanner = ({
+  bannerData: {
+    smallText,
+    midText,
+    largeText1,
+    desc,
+    product,
+    buttonText,
+    image,
+  },
+}: Props) => {
+  const imageProps = useNextSanityImage(client, image);
 
   return (
     <div className="h-[720px] bg-neutral-300 text-center pt-5 flex flex-col items-center sm:h-[800px] md:items-start md:grid md:mt-20 md:rounded-2xl md:text-left md:grid-cols-6 md:grid-rows-6 md:px-10 md:h-[500px]">
       <div className="leading-[.8] text-left md:col-start-1 md:col-end-5 md:row-start-2 md:row-end-5 lg:row-end-6">
         <h3 className="text-center text-neutral-500 text-2xl ml-1.5 md:text-left lg:text-3xl">
-          {bannerData.smallText}
+          {smallText}
         </h3>
         <h3 className="text-center font-bold ml-1 text-4xl mt-5 md:mt-0 md:text-left md:text-7xl lg:text-8xl">
-          {bannerData.midText}
+          {midText}
         </h3>
         <h3 className="text-center font-bold text-neutral-50 tracking-tighter leading-[.8] text-9xl md:text-left md:text-[160px] lg:text-[200px]">
-          {bannerData.largeText1}
+          {largeText1}
         </h3>
       </div>
       <div className="h-80 w-80 mt-5 sm:h-[400px] sm:w-[400px] md:col-start-2 md:col-end-7 md:justify-self-end lg:col-start-3 lg:justify-self-center">
@@ -34,16 +44,16 @@ const HeroBanner = ({ bannerData }: Props) => {
       </div>
       <div className="flex flex-col md:col-start-2 md:col-end-7 md:justify-self-end md:mr-10 md:row-start-6 md:row-end-7 lg:ml-10 lg:col-start-3 lg:col-end-7 lg:justify-self-center">
         <h3 className="text-2xl md:text-xl lg:text-2xl text-neutral-500">
-          {bannerData.desc}
+          {desc}
         </h3>
       </div>
       <div className="col-start-1 col-end-4 row-start-5 row-end-6 lg:row-start-6 lg:row-end-7">
-        <Link href={`/product/${bannerData.product}`}>
+        <Link href={`/product/${product}`}>
           <button
             type="button"
             className="rounded-full py-2 px-7 mt-10 ml-1.5 text-neutral-50 bg-red-500 text-2xl font-semibold transition-transform ease-in-out duration-500 hover:scale-110 hover:bg-red-600 lg:mt-0"
           >
-            {bannerData.buttonText}
+            {buttonText}
           </button>
         </Link>
       </div>
