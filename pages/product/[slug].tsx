dcced6818585832196fb160547e8703e20af208a
Shaun Carr <shaunc138@gmail.com>
2022-08-20 01:29:11 -0400

    add or subtract item section styles

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { client, urlFor } from "../../lib/client";
 import { Product } from "../../typings";
-import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import {
+  AiFillStar,
+  AiOutlineStar,
+  AiOutlineMinus,
+  AiOutlinePlus,
+} from "react-icons/ai";
 
 type Products = Product[];
 
@@ -54,7 +59,7 @@ export const ProductPage = ({ product, products }: Props) => {
   const [index, setIndex] = useState(0);
 
   return (
-    <div className="mt-10">
+    <div className="mt-10 text-neutral-600">
       <div className="flex flex-col items-center">
         <div className="h-80 w-80 bg-neutral-200 rounded-xl hover:bg-red-500 transition-colors ease-in-out duration-300">
           <img
@@ -77,16 +82,33 @@ export const ProductPage = ({ product, products }: Props) => {
         </div>
       </div>
       <div className="mt-10 flex flex-col items-center">
-        <h2 className="w-80 font-bold text-3xl text-neutral-600">
-          {product.name}
-        </h2>
+        <h2 className="w-80 font-bold text-3xl">{product.name}</h2>
         <div className="flex items-center w-80 text-red-500 mt-2">
           <AiFillStar />
           <AiFillStar />
           <AiFillStar />
           <AiFillStar />
           <AiOutlineStar />
-          <p className="ml-2 text-black">(20)</p>
+          <p className="ml-2 text-neutral-600">(20)</p>
+        </div>
+        <h3 className="w-80 mt-2 font-bold text-lg">Details:</h3>
+        <p className="w-80 mt-2">{product.details}</p>
+        <h3 className="w-80 mt-5 font-bold text-3xl text-red-500">
+          ${product.price}
+        </h3>
+        <div className="flex items-center w-80 mt-5">
+          <h3 className="font-bold text-xl mr-5">Quantity:</h3>
+          <div className="border flex border-neutral-600">
+            <div className="flex items-center justify-center px-3 py-2">
+              <AiOutlineMinus />
+            </div>
+            <div className="flex items-center justify-center px-4 py-2 border-x border-neutral-600">
+              <p className="text-2xl">1</p>
+            </div>
+            <div className="flex items-center justify-center px-3 py-2">
+              <AiOutlinePlus />
+            </div>
+          </div>
         </div>
       </div>
     </div>
