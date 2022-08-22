ab19a0ff36633b8dbd614137f6fd343fa5aa9d2a
Shaun Carr <shaunc138@gmail.com>
2022-08-21 23:45:03 -0400

    Begin building StateContext

---
+++ context/StateContext.tsx
@@ -0,0 +1,53 @@
+import React, { useState, createContext, useContext, ReactNode } from "react";
+import { Product } from "../typings";
+import { toast } from "react-hot-toast";
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface CartItem extends Product {
+  quantity: number;
+}
+
+interface StateContextInterface {
+  cartItems: CartItem[];
+  setCartItems: React.Dispatch<React.SetStateAction<never[]>>;
+  showCart: boolean;
+  setShowCart: React.Dispatch<React.SetStateAction<boolean>>;
+  totalPrice: number;
+  setTotalPrice: React.Dispatch<React.SetStateAction<number>>;
+  totalQuantitys: number;
+  setTotalQuantitys: React.Dispatch<React.SetStateAction<number>>;
+  qty: number;
+}
+
+const Context = createContext<StateContextInterface | null>(null);
+
+export const StateContext = ({ children }: Props) => {
+  const [cartItems, setCartItems] = useState([]);
+  const [showCart, setShowCart] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantitys, setTotalQuantitys] = useState(0);
+  const [qty, setQty] = useState(1);
+
+  return (
+    <Context.Provider
+      value={{
+        cartItems,
+        setCartItems,
+        showCart,
+        setShowCart,
+        totalPrice,
+        setTotalPrice,
+        totalQuantitys,
+        setTotalQuantitys,
+        qty,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(Context);
