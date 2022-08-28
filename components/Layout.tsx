0174aae969dbc83c2275d80e61675b579bbe6065
Shaun Carr <shaunc138@gmail.com>
2022-08-28 03:25:43 -0400

    Add favicons to _app

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
@@ -9,9 +8,6 @@ interface Props {
 const Layout = ({ children }: Props) => {
   return (
     <div className="min-h-screen bg-neutral-50">
-      <Head>
-        <title>AccessoryShack</title>
-      </Head>
       <Navbar />
       <main className="md:mx-10 lg:mx-32">{children}</main>
       <Footer />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,35 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Layout from "../components/Layout";
 import { StateContext } from "../context/StateContext";
 import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <StateContext>
-      <Layout>
-        <Toaster />
-        <Component {...pageProps} />
-      </Layout>
-    </StateContext>
+    <>
+      <Head>
+        <title>AccessoryShack</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+      </Head>
+      <StateContext>
+        <Layout>
+          <Toaster />
+          <Component {...pageProps} />
+        </Layout>
+      </StateContext>
+    </>
   );
 }
 
